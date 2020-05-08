65231893aa6490de0783e5781d2982781381390a
Vanchurick <davane4ek@gmail.com>
2020-05-08 13:49:03 +0300

    added cors

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 const router = require("./routes/router");
 
 const startServer = (port) => {
   app.use(express.json());
+  app.use(cors());
 
   app.use("/api", router);
 
